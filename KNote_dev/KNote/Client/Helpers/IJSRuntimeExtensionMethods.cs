1f3fe58adb8f2e74528b4bd482634f1179885882
Armando Fumero Fernández <afumfer@gmail.com>
2020-01-09 19:40:54 +0000

    [X]  Maqueta inicial  20200109

--- KNote_dev/KNote/Client/Helpers/IJSRuntimeExtensionMethods.cs
+++ KNote_dev/KNote/Client/Helpers/IJSRuntimeExtensionMethods.cs
@@ -14,19 +14,19 @@ namespace KNote.Client.Helpers
             return await js.InvokeAsync<bool>("confirm", mensaje);
         }
 
-        public static ValueTask<object> SetInLocalStorage(this IJSRuntime js, string key, string content)
+        public static ValueTask<object> LocalStorageSetItem(this IJSRuntime js, string key, string content)
            => js.InvokeAsync<object>(
                "localStorage.setItem",
                key, content
                );
 
-        public static ValueTask<string> GetFromLocalStorage(this IJSRuntime js, string key)
+        public static ValueTask<string> LocalStorageGetItem(this IJSRuntime js, string key)
             => js.InvokeAsync<string>(
                 "localStorage.getItem",
                 key
                 );
 
-        public static ValueTask<object> RemoveItem(this IJSRuntime js, string key)
+        public static ValueTask<object> LocalStorageRemoveItem(this IJSRuntime js, string key)
             => js.InvokeAsync<object>(
                 "localStorage.removeItem",
                 key);
