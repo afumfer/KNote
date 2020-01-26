a9bf646db4c5248b795fb14b80e441b52b2fe619
Armando Fumero Fernández <afumfer@gmail.com>
2020-01-26 21:03:45 +0000

    [X] User Index

--- KNote_dev/KNote/Client/ClientDataServices/IKntClientDataService.cs
+++ KNote_dev/KNote/Client/ClientDataServices/IKntClientDataService.cs
@@ -8,7 +8,11 @@ namespace KNote.Client.ClientDataServices
 {
     public interface IKntClientDataService
     {
+        Task<HttpResponseWrapper<object>> Delete(string url);
+        Task<HttpResponseWrapper<T>> Get<T>(string url);
         List<NoteInfoDto> GetNotes();
         Task<HttpResponseWrapper<object>> Post<T>(string url, T enviar);
+        Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T enviar);
+        Task<HttpResponseWrapper<object>> Put<T>(string url, T enviar);
     }
 }
--- KNote_dev/KNote/Client/ClientDataServices/KntClientDataService.cs
+++ KNote_dev/KNote/Client/ClientDataServices/KntClientDataService.cs
@@ -18,6 +18,21 @@ namespace KNote.Client.ClientDataServices
             this.httpClient = httpClient;
         }
 
+        public async Task<HttpResponseWrapper<T>> Get<T>(string url)
+        {
+            var responseHTTP = await httpClient.GetAsync(url);
+
+            if (responseHTTP.IsSuccessStatusCode)
+            {
+                var response = await DeserializeResponse<T>(responseHTTP, DefaultJSONOptions);
+                return new HttpResponseWrapper<T>(response, false, responseHTTP);
+            }
+            else
+            {
+                return new HttpResponseWrapper<T>(default, true, responseHTTP);
+            }
+        }
+
         public async Task<HttpResponseWrapper<object>> Post<T>(string url, T enviar)
         {
             var sendJSON = JsonSerializer.Serialize(enviar);
@@ -26,7 +41,50 @@ namespace KNote.Client.ClientDataServices
             return new HttpResponseWrapper<object>(null, !responseHttp.IsSuccessStatusCode, responseHttp);
         }
 
+        public async Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T enviar)
+        {
+            var enviarJSON = JsonSerializer.Serialize(enviar);
+            var enviarContent = new StringContent(enviarJSON, Encoding.UTF8, "application/json");
+            var responseHttp = await httpClient.PostAsync(url, enviarContent);
+            if (responseHttp.IsSuccessStatusCode)
+            {
+                var response = await DeserializeResponse<TResponse>(responseHttp, DefaultJSONOptions);
+                return new HttpResponseWrapper<TResponse>(response, false, responseHttp);
+            }
+            else
+            {
+                return new HttpResponseWrapper<TResponse>(default, true, responseHttp);
+            }
+        }
 
+        public async Task<HttpResponseWrapper<object>> Put<T>(string url, T enviar)
+        {
+            var sendJSON = JsonSerializer.Serialize(enviar);
+            var sendContent = new StringContent(sendJSON, Encoding.UTF8, "application/json");
+            var responseHttp = await httpClient.PutAsync(url, sendContent);
+            return new HttpResponseWrapper<object>(null, !responseHttp.IsSuccessStatusCode, responseHttp);
+        }
+
+        public async Task<HttpResponseWrapper<object>> Delete(string url)
+        {
+            var responseHTTP = await httpClient.DeleteAsync(url);
+            return new HttpResponseWrapper<object>(null, !responseHTTP.IsSuccessStatusCode, responseHTTP);
+        }
+
+        #region Utils
+
+        private async Task<T> DeserializeResponse<T>(HttpResponseMessage httpResponse, JsonSerializerOptions jsonSerializerOptions)
+        {
+            var responseString = await httpResponse.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(responseString, jsonSerializerOptions);
+        }
+
+        private JsonSerializerOptions DefaultJSONOptions =>
+            new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+
+        #endregion 
+
+        // TODO: !!! Borrar .... 
         public List<NoteInfoDto> GetNotes()
         {
             return new List<NoteInfoDto>()
