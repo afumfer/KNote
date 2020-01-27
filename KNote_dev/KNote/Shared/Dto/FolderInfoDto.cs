b4480687fa122c9a5bbe32e4f157ef727e25a082
Armando Fumero Fernández <afumfer@gmail.com>
2020-01-27 19:22:48 +0000

    [X] FoldersIndex

--- KNote_dev/KNote/Shared/Dto/FolderInfoDto.cs
+++ KNote_dev/KNote/Shared/Dto/FolderInfoDto.cs
@@ -22,5 +22,51 @@ namespace KNote.Shared.Dto
         // TODO: Icon es provisional, 
         public string Icon { get; set; } = "fa-folder";
         public List<FolderInfoDto> ChildFolders { get; set; } = new List<FolderInfoDto>();
+
+        public string ShortName
+        {
+            get
+            {
+                if (Name.Length > 45)
+                    return Name.Substring(0, 42) + "...";
+                else
+                    return Name;
+            }
+        }
+
+        private bool _expanded = false;
+        public bool Expanded
+        {
+            get { return _expanded; }
+        }
+
+        public void Toggle()
+        {
+            _expanded = !_expanded;
+        }
+
+        public string getIcon()
+        {
+            if (_expanded)
+            {
+                //return "-";
+                return "oi oi-caret-bottom";
+                //return "oi oi-chevron-bottom";
+            }
+
+            //return "+";
+            return "oi oi-caret-right";
+            //return "oi oi-chevron-right";
+        }
+
+        public bool Selected { get; set; } = false;
+
+        public string getColor()
+        {
+            if (Selected)
+                return "text-light bg-dark";
+            return "text-dark";
+
+        }
     }
 }
