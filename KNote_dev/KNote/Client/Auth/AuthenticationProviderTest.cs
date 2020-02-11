416a295f366a610f17593375dd8d47ec12dc51aa
Armando Fumero Fernández <afumfer@gmail.com>
2020-02-11 18:45:00 +0000

    [X]  Test de autorización / autenticación en el cliente (fase 0)

--- KNote_dev/KNote/Client/Auth/AuthenticationProviderTest.cs
+++ KNote_dev/KNote/Client/Auth/AuthenticationProviderTest.cs
@@ -11,10 +11,21 @@ namespace KNote.Client.Auth
     {
         public async override Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            await Task.Delay(5000);
+            //await Task.Delay(5000);
 
-            var anonimo = new ClaimsIdentity();
+            //var anonimo = new ClaimsIdentity();
+
+            var anonimo = new ClaimsIdentity(new List<Claim>() {
+                new Claim("llave1", "valor1")
+                ,new Claim(ClaimTypes.Name, "Armando")
+                ,new Claim(ClaimTypes.Role, "Admin")
+                ,new Claim(ClaimTypes.Role, "ProjecManager")
+                ,new Claim(ClaimTypes.Role, "Staff")
+            }, "test");
+
             return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonimo)));
+
+
         }
     }
 }
