4478423951ebf2a94930941227c251e2a409562c
Armando Fumero Fernández <afumfer@gmail.com>
2020-04-18 23:08:14 +0100

    [X] User -> Roles (Interfaz de Usuario)

---
+++ KNote_dev/KNote/Shared/Dto/MultiSelectListDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KNote.Shared.Dto
+{
+    public class MultiSelectListDto
+    {
+        public MultiSelectListDto(bool selected, string key, string value)
+        {
+            Selected = selected;
+            Key = key;
+            Value = value;
+        }
+
+        public bool Selected { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
--- KNote_dev/KNote/Shared/Dto/PaginationDto.cs
+++ KNote_dev/KNote/Shared/Dto/PaginationDto.cs
@@ -7,7 +7,7 @@ namespace KNote.Shared.Dto
     public class PaginationDto
     {
         public int Page { get; set; } = 1;
-        public int NumRecords { get; set; } = 10;
+        public int NumRecords { get; set; } = 25;
 
         // PageRequest
     }
