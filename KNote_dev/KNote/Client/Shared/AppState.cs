6bd2211ba0c27dccff497aa1c34870d0a5e0f180
Armando Fumero Fernández <afumfer@gmail.com>
2020-05-06 23:09:41 +0100

    [X] Inicio versión alfa 0.0.4.2

--- KNote_dev/KNote/Client/Shared/AppState.cs
+++ KNote_dev/KNote/Client/Shared/AppState.cs
@@ -14,7 +14,7 @@ namespace KNote.Client.Shared
             set { _appMode = value; NotifyStateChanged(); }
         }
 
-        private string _appName = "Other keynotes management for people's group (ver 0.0.4.1)";
+        private string _appName = "Other keynotes management for people's group (ver 0.0.4.2)";
         public string AppName
         {
             get { return _appName; }
