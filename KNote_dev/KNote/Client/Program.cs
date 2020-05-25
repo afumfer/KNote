75c015c7029f7a8658903da43e4e51db7556d55e
Armando Fumero Fernández <afumfer@gmail.com>
2020-05-25 23:04:32 +0100

    [X] AppState. (1)

--- KNote_dev/KNote/Client/Program.cs
+++ KNote_dev/KNote/Client/Program.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Components.Authorization;
 using KNote.Client.Auth;
 using Blazor.FileReader;
 using System.Net.Http;
+using KNote.Client.Shared;
 
 namespace KNote.Client
 {    
@@ -22,13 +23,16 @@ namespace KNote.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            // builder.Services.AddBaseAddressHttpClient();  // old 1
-            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });  // old 2
-            //builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });    // rc 1 OJO ESTO NO FUNCIONA
+            // old 1
+            // builder.Services.AddBaseAddressHttpClient();  
+            // old 2
+            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            // rc 1 OJO ESTO NO FUNCIONA
+            //builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });    
 
             builder.Services.AddOptions();
             builder.Services.AddScoped<IShowMessages, ShowMessages>();
-            builder.Services.AddScoped<IKntClientDataService, KntClientDataService>();
+            builder.Services.AddScoped<IKntClientDataService, KntClientDataService>();            
             builder.Services.AddAuthorizationCore();
 
             // Test ...
--- KNote_dev/KNote/Client/Shared/AppState.cs
+++ KNote_dev/KNote/Client/Shared/AppState.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KNote.Shared.Dto;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -29,25 +30,26 @@ namespace KNote.Client.Shared
         }
 
 
-        private Guid _selectedFolder;
-        public Guid SelectedFolder
+        private FolderDto _selectedFolder;
+        public FolderDto SelectedFolder
         {
-            get { return _selectedFolder; }
-            set { _selectedFolder = value; NotifyStateChanged(); }
+            get 
+            {
+                if (_selectedFolder == null)
+                    _selectedFolder = new FolderDto();
+                return _selectedFolder; 
+            }
+            set 
+            { 
+                _selectedFolder = value; 
+                NotifyStateChanged(); 
+            }
         }
 
-        // TODO: !!! pendiente de eliminar 
-        private string _textSearch;
-        public string TextSearch
-        {
-            get { return _textSearch; }
-            set { _textSearch = value; NotifyStateChanged(); }
-        }
 
         public event Action OnChange;
         private void NotifyStateChanged() => OnChange?.Invoke();
 
-        
 
     }
 }
