1df9e65100f9c029069cdf329f8d5abd4ee4f7f6
Armando Fumero Fernández <afumfer@gmail.com>
2020-06-07 18:54:19 +0100

    [X] Transformar en un control la cabecera de título de página y botonera (new). [X] Estandarizar form mantenimiento types.

--- KNote_dev/KNote/Client/Shared/AppState.cs
+++ KNote_dev/KNote/Client/Shared/AppState.cs
@@ -22,7 +22,7 @@ namespace KNote.Client.Shared
             set { _appName = value; NotifyStateChanged(); }
         }
 
-        private string _appDescription = "Other keynotes management for people's group (ver 0.0.4.3)";
+        private string _appDescription = "Other keynotes management for people's group (ver 0.0.4.4)";
         public string AppDescription
         {
             get { return _appDescription; }
