48ea611bebb3967669506ab25e92d32c4cfbd83f
Armando Fumero Fernández <afumfer@gmail.com>
2021-06-06 22:30:28 +0100

    Init version 0.0.5.16

--- Src/KNote/Client/Shared/AppState.cs
+++ Src/KNote/Client/Shared/AppState.cs
@@ -23,7 +23,7 @@ namespace KNote.Client.Shared
             set { _appName = value; NotifyStateChanged(); }
         }
 
-        private string _appDescription = "Another keynotes managment (ver 0.0.5.15)";
+        private string _appDescription = "Another keynotes managment (ver 0.0.5.16)";
         public string AppDescription
         {
             get { return _appDescription; }
