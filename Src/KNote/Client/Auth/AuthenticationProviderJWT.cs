21ee68154910bfc5611931f8857bef2f9ece2706
Armando Fumero Fernández <afumfer@gmail.com>
2023-09-20 19:57:57 +0100

    Code Cleanup

--- Src/KNote/Client/Auth/AuthenticationProviderJWT.cs
+++ Src/KNote/Client/Auth/AuthenticationProviderJWT.cs
@@ -37,8 +37,7 @@ public class AuthenticationProviderJWT : AuthenticationStateProvider, ILoginServ
     }
 
     public AuthenticationState BuildAuthenticationState(string token)
-    {
-        //AuthenticationState authState = null!;
+    {        
         httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);       
         var authState = new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt")));        
         store.AppState.UserName = authState?.User?.Identity?.Name;
@@ -51,51 +50,6 @@ public class AuthenticationProviderJWT : AuthenticationStateProvider, ILoginServ
         var tokenDeserialized = jwtSecurityTokenHandler.ReadJwtToken(token);
         return tokenDeserialized.Claims;
     }
-
-    //private IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
-    //{
-    //    var claims = new List<Claim>();
-    //    var payload = jwt.Split('.')[1];
-    //    var jsonBytes = ParseBase64WithoutPadding(payload);
-    //    var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
-
-    //    keyValuePairs.TryGetValue(ClaimTypes.Role, out object roles);
-
-    //    if (roles != null)
-    //    {
-    //        if (roles.ToString().Trim().StartsWith("["))
-    //        {
-    //            var parsedRoles = JsonSerializer.Deserialize<string[]>(roles.ToString());
-
-    //            foreach (var parsedRole in parsedRoles)
-    //            {
-    //                claims.Add(new Claim(ClaimTypes.Role, parsedRole));
-    //            }
-    //        }
-    //        else
-    //        {
-    //            claims.Add(new Claim(ClaimTypes.Role, roles.ToString()));
-    //        }
-
-    //        keyValuePairs.Remove(ClaimTypes.Role);
-    //    }
-
-    //    claims.AddRange(keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value?.ToString())));
-    //    return claims;
-    //}
-
-    //private byte[] ParseBase64WithoutPadding(string base64)
-    //{
-    //    switch (base64.Length % 4)
-    //    {
-    //        case 2: base64 += "=="; break;
-    //        case 3: base64 += "="; break;
-    //    }
-    //    return Convert.FromBase64String(base64);
-    //}
-
-
-
 
     public async Task Login(string token)
     {
