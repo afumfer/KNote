72061924be30f3ba69c22df75e7c1f593a7f564d
Armando Fumero Fernández <afumfer@gmail.com>
2024-11-01 18:06:28 +0000

    Updatee nuget packages in Test Project. Updated chat test.

--- Src/KNote/Tests/WebApiIntegrationTests/ChatGPTTests.cs
+++ Src/KNote/Tests/WebApiIntegrationTests/ChatGPTTests.cs
@@ -16,12 +16,12 @@ public class ChatGPTTests : WebApiTestBase
 
     [TestMethod]
     public async Task Post_ReturnBasicChatMessage()
-    {        
-        List<ChatMessage> chatMessages = new List<ChatMessage>();
-        chatMessages.Add(new ChatMessage { Role = "user", Prompt = "Hello." });
+    {
+        List<KntChatMessage> chatMessages = new List<KntChatMessage>();
+        chatMessages.Add(new KntChatMessage { Role = "user", Prompt = "Hello." });
 
         var httpRes = await HttpClient.PostAsJsonAsync($"{UrlBase}api/chatgpt", chatMessages);
-        var res = await ProcessResultFromTestHttpResponse<ChatMessageOutput>(httpRes);
+        var res = await ProcessResultFromTestHttpResponse<KntChatMessageOutput>(httpRes);
 
         Assert.IsTrue(res.IsValid);
         Assert.IsTrue(res.Entity.Prompt.Length > 0);
